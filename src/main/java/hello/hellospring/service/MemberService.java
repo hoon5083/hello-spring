af97f1ab3654c87254ec2836313ca342c2f6193b
Yikanghoon <hoon5083@gmail.com>
2023-07-18 06:08:34 +0900

    feat: 서비스 코드 레포지토리 DI 가능하게 변경

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,16 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new
-            MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
 
     public Long join(Member member) {
@@ -24,7 +26,7 @@ public class MemberService {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
-    
+
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
